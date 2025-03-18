ac85677491a24a439af149bf3399fb24cf9dcb34
ujjwal basnyat <asujjwalbasnyat@gmail.com>
2025-03-19 00:15:23 +0545

    fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "../../components/nav";
-import Footer from "../../components/footer";
+import Navbar from "../components/nav";
+import Footer from "../components/footer";
 
 
 export const metadata: Metadata = {
