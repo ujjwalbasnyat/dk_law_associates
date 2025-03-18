0f2fb9ffe26b40787b3aedbf69e505d37c693826
ujjwal basnyat <asujjwalbasnyat@gmail.com>
2025-03-19 00:23:43 +0545

    resolve build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "../components/nav";
-import Footer from "../components/footer";
+import Navbar from "./nav";
+import Footer from "./footer";
 
 
 export const metadata: Metadata = {
